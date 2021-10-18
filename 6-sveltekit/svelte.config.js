bfbf28e76e4c5d1d84dbe42cdc4925a2b000587d
adambar <hi@adambar.pl>
2021-10-18 15:32:51 +0200

    serve sveltekit

--- 6-sveltekit/svelte.config.js
+++ 6-sveltekit/svelte.config.js
@@ -1,9 +1,7 @@
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
-};
+import adapter from '@sveltejs/adapter-static';
 
-export default config;
+export default {
+    kit: {
+        adapter: adapter()
+    }
+};
--- heroku-build.sh
+++ heroku-build.sh
@@ -6,3 +6,4 @@ yarn --cwd 2-pure-spa install && yarn --cwd 2-pure-spa build
 npm --prefix 3-ssg-gatsby install --production=false && npm --prefix 3-ssg-gatsby run build
 npm --prefix 4-ssr-next install --production=false && npm --prefix 4-ssr-next run build
 npm --prefix 5-dynamic-rendering install --production=false && npm --prefix 5-dynamic-rendering run build
+npm --prefix 6-sveltekit install --production=false && npm --prefix 6-sveltekit run build
