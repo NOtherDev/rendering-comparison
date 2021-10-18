808a6875bb538f615feab3c142fa44632bac9c66
adambar <hi@adambar.pl>
2021-10-18 13:30:53 +0200

    include devDependencies

--- heroku-build.sh
+++ heroku-build.sh
@@ -3,7 +3,6 @@
 npm install --global yarn
 npm --prefix 1-pure-html install && npm --prefix 1-pure-html run build
 yarn --cwd 2-pure-spa install && yarn --cwd 2-pure-spa build
-npm --prefix 3-ssg-gatsby install
-npm --prefix 3-ssg-gatsby run build
-npm --prefix 4-ssr-next install && npm --prefix 4-ssr-next run build
-npm --prefix 5-dynamic-rendering install && npm --prefix 5-dynamic-rendering run build
+npm --prefix 3-ssg-gatsby install && npm --prefix 3-ssg-gatsby run build
+npm --prefix 4-ssr-next install --production=false && npm --prefix 4-ssr-next run build
+npm --prefix 5-dynamic-rendering install --production=false && npm --prefix 5-dynamic-rendering run build
