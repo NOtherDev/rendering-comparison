0412e734c3d203c5b10626bfc0826987ccf3021c
adambar <hi@adambar.pl>
2021-10-18 15:49:44 +0200

    fix yarn ci

--- heroku-build.sh
+++ heroku-build.sh
@@ -2,7 +2,7 @@
 
 npm install --global yarn
 npm --prefix 1-pure-html ci && npm --prefix 1-pure-html run build
-yarn --cwd 2-pure-spa ci && yarn --cwd 2-pure-spa build
+yarn --cwd 2-pure-spa install && yarn --cwd 2-pure-spa build
 npm --prefix 3-ssg-gatsby ci --production=false && npm --prefix 3-ssg-gatsby run build
 npm --prefix 4-ssr-next ci --production=false && npm --prefix 4-ssr-next run build
 npm --prefix 5-dynamic-rendering ci --production=false && npm --prefix 5-dynamic-rendering run build
