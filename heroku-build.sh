4a3613eafe88b7fd32a841856c7c5ac1029c8e4a
adambar <hi@adambar.pl>
2021-10-18 10:05:36 +0200

    move to build

---
+++ heroku-build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+npm --prefix 1-pure-html install && npm --prefix 1-pure-html build
+yarn --cwd 2-pure-spa install && yarn --cwd 2-pure-spa build
+npm --prefix 3-ssg-gatsby install && npm --prefix 3-ssg-gatsby build
+npm --prefix 4-ssr-next install && npm --prefix 4-ssr-next build
+npm --prefix 5-dynamic-rendering install && npm --prefix 5-dynamic-rendering build
