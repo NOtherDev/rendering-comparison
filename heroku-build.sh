3a56962b4e352e37687896b7b70b263faff49bb0
adambar <hi@adambar.pl>
2021-10-18 13:17:10 +0200

    gatsby debug

--- heroku-build.sh
+++ heroku-build.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-npm install --global yarn
-npm --prefix 1-pure-html install && npm --prefix 1-pure-html build
-yarn --cwd 2-pure-spa install && yarn --cwd 2-pure-spa build
-npm --prefix 3-ssg-gatsby install && npm --prefix 3-ssg-gatsby build
-npm --prefix 4-ssr-next install && npm --prefix 4-ssr-next build
-npm --prefix 5-dynamic-rendering install && npm --prefix 5-dynamic-rendering build
+#npm install --global yarn
+#npm --prefix 1-pure-html install && npm --prefix 1-pure-html build
+#yarn --cwd 2-pure-spa install && yarn --cwd 2-pure-spa build
+npm --prefix 3-ssg-gatsby install
+npm --verbose --prefix 3-ssg-gatsby build
+#npm --prefix 4-ssr-next install && npm --prefix 4-ssr-next build
+#npm --prefix 5-dynamic-rendering install && npm --prefix 5-dynamic-rendering build
